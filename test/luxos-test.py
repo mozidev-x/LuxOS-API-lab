8ec4eb95c47ed87d2b6abf533cf5a2bf1af932fc
Mozi <mozi.m@outlook.com>
2025-10-06 16:37:54 -0500

    Update luxos-test.py

--- test/luxos-test.py
+++ test/luxos-test.py
@@ -1,19 +1,21 @@
 import socket
-import time
-import threading
 
-# act as miner
 server_addr = "0.0.0.0"
 
 def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((server_addr, 4028))
-    print("Listening on port 4028...")
+    s.listen(5)
+    print(f"Listening on TCP port 4028...")
+
     while True:
-        data, addr = s.recvfrom(1024)
-        print(f"Received message: {data} from {addr}")
-    
+        conn, addr = s.accept()
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(f"{data.decode()}")
+        conn.close()
 
 if __name__ == "__main__":
-
     main()
